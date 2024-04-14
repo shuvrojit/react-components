9728547ba9c4bfb4390f8049476068e420861c53
Shuvrojit Biswas <jsuvro17@gmail.com>
2024-04-14 21:39:16 +0600

    Add CSS reset from eric mayer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import './App.css'
+import "./App.css";
+import Nav from "./components/navbar/index";
+import Subscription from "./components/subscription/Subscription";
 
 function App() {
-
   return (
     <>
+      <Nav.HamburgerMenu direction="left" />
+      <Nav.HamburgerMenu direction="right" position="bottom-right" />
+      <Nav.HamburgerMenu direction="right" position="top-right" />
+      <Nav.HamburgerMenu direction="left" position="bottom-left" />
+      <Subscription />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
