ebb0bec3478f0c6a11283e60c03c723d5e3e4133
Shuvrojit Biswas <jsuvro17@gmail.com>
2024-05-09 09:50:00 +0600

    Add contact form
    Created a reusable contact form component with raw css.

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,15 @@ import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Header from "./components/header/Header";
+import ContactForm from "./components/body/ContactForm.tsx";
+
+const formDescriptor =
+  "If you have any questions or concerns, please feel free to contact us. We will get back to you as soon as possible.";
+
+const contactImageSrc =
+  "https://plus.unsplash.com/premium_photo-1682310473661-7a334b46e164?q=80&w=1512&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+const contactImageAlt = "Illustration of a man sitting infront of e-mails";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +23,16 @@ const router = createBrowserRouter([
   {
     path: "/header",
     element: <Header />,
+  },
+  {
+    path: "/contact",
+    element: (
+      <ContactForm
+        formDescription={formDescriptor}
+        imageSrc={contactImageSrc}
+        imageAlt={contactImageAlt}
+      />
+    ),
   },
 ]);
 
